b9723a253d2facabac592f7cf896e02f1257355d
LUCKDG1 <127677566+LUCKDG1@users.noreply.github.com>
2023-03-12 20:56:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("my first console screen project");
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+var a = 10;
+console.log(a);
+var b = 5;
+console.log(b);
+var add = a + b ;
+console.log(add);
